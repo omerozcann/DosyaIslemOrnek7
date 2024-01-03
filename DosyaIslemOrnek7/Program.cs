bdaa53a8a3662b620414221560cb5771e7ce788a
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2024-01-03 11:06:44 +0300

    Proje dosyası ekle.

---
+++ DosyaIslemOrnek7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DosyaIslemOrnek7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizilerin Dosyaya Yazılması / Okunması
+
+            String[] veriler = { "Ahmet", "Hasan", "Ayşe", "Deniz", "Merve" };
+            File.WriteAllLines("kayitlar.txt", veriler); //Dizinin elemanlarını dosyaya yaz
+
+            String[] d;
+            d = File.ReadAllLines("kayitlar.txt");//dosyadaki satırları oku d dizisine aktar
+            for (int i = 0; i < d.Length; i++)
+            {
+                Console.WriteLine(d[i]);
+            }
+        }
+    }
+}
